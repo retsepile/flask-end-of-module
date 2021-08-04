948bfdd2d79a92987fcb3e233b6593cced50b374
retsepile <Retshepilekoloko27@gmail.com>
2021-08-04 13:42:14 +0200

    update

--- backend/app.py
+++ backend/app.py
@@ -4,19 +4,63 @@ import datetime
 
 from flask import Flask, request, jsonify
 from flask_jwt import JWT, jwt_required, current_identity
+from flask_cors import CORS
 
 
 class User(object):
-    def __init__(self, id, username, password):
+    def __init__(self, id, username, password, user_email, phone_number, address):
         self.id = id
         self.username = username
         self.password = password
+        self.user_email = user_email
+        self.phone_number = phone_number
+        self.address = address
 
-# database and user selection
+# Initialisation of the users table
 
 
-def sign_in_users():
-    with sqlite3.connect('products.db') as conn:
+def user_table():
+    conn = sqlite3.connect('POS.db')
+    print("Opened database successfully")
+
+    conn.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 "first_name TEXT NOT NULL,"
+                 "last_name TEXT NOT NULL,"
+                 "username TEXT NOT NULL,"
+                 "password TEXT NOT NULL, address TEXT NOT NULL, phone_number INT NOT NULL, user_email TEXT NOT NULL)")
+    print("user table created successfully")
+    conn.close()
+
+# users that logged in
+
+
+def login_table():
+    with sqlite3.connect('POS.db') as conn:
+        conn.execute("CREATE TABLE IF NOT EXISTS login (id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                     "user_email TEXT NOT NULL,"
+                     "password TEXT NOT NULL,"
+                     "login_date TEXT NOT NULL)")
+    print("Login table created successfully.")
+
+
+def products_table():
+    with sqlite3.connect('POS.db') as conn:
+        conn.execute("CREATE TABLE IF NOT EXISTS products_table (id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                     "product_name TEXT NOT NULL,"
+                     "price TEXT NOT NULL,"
+                     "description TEXT NOT NULL)")
+    print("Product table created successfully.")
+
+
+products_table()
+user_table()
+login_table()
+
+# fetching the users from the user table
+
+
+def fetch_users():
+    with sqlite3.connect('POS.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM user")
         users = cursor.fetchall()
@@ -24,58 +68,16 @@ def sign_in_users():
         new_data = []
 
         for data in users:
-            new_data.append(User(data[0], data[3], data[4]))
+            new_data.append(User(data[0], data[3], data[4], data[5], data[6], data[7]))
     return new_data
 
 
-users = sign_in_users()
-
-# initialisation of the user table
-
-
-def signed_in_users_table():
-    conn = sqlite3.connect('products.db')
-    print("Opened database successfully")
-
-    conn.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                 "first_name TEXT NOT NULL,"
-                 "last_name TEXT NOT NULL,"
-                 "username TEXT NOT NULL,"
-                 "password TEXT NOT NULL)")
-    print("user table created successfully")
-    conn.close()
-# registration of new members
-
-
-def registration_table():
-    with sqlite3.connect('products.db') as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS user_registration(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                     "first_name TEXT NOT NULL,"
-                     "last_name TEXT NOT NULL,"
-                     "username TEXT NOT NULL,"
-                     "password TEXT NOT NULL)")
-        print("register table created successfully")
-
-
-# initialisation of the product table
-
-
-def product_table():
-    with sqlite3.connect('products.db') as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS product (id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                     "title TEXT NOT NULL,"
-                     "name TEXT NOT NULL,"
-                     "price INT NOT NULL"
-                     "quantity INT NOT NULL)")
-    print("product table created successfully.")
-
-
-signed_in_users_table()
-registration_table()
-product_table()
+users = fetch_users()
 
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
+
+# the process  of verifying the identity of a user.
 
 
 def authenticate(username, password):
@@ -90,10 +92,13 @@ def identity(payload):
 
 
 app = Flask(__name__)
+CORS(app)
 app.debug = True
 app.config['SECRET_KEY'] = 'super-secret'
 
 jwt = JWT(app, authenticate, identity)
+
+# when the user enters they details it will be protected from the third party
 
 
 @app.route('/protected')
@@ -101,9 +106,11 @@ jwt = JWT(app, authenticate, identity)
 def protected():
     return '%s' % current_identity
 
+# creating a function for people who would want to register for an account
+
 
 @app.route('/user-registration/', methods=["POST"])
-def user_registration():
+def new_member_registration():
     response = {}
 
     if request.method == "POST":
@@ -112,50 +119,70 @@ def user_registration():
         last_name = request.form['last_name']
         username = request.form['username']
         password = request.form['password']
+        address = request.form['address']
+        phone_number = request.form['phone_number']
+        user_email = request.form['user_email']
 
-        with sqlite3.connect("products.db") as conn:
+        with sqlite3.connect("POS.db") as conn:
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO user_registration("
+            cursor.execute("INSERT INTO user("
                            "first_name,"
                            "last_name,"
                            "username,"
-                           "password) VALUES(?, ?, ?, ?)", (first_name, last_name, username, password))
+                           "password,address,phone_number,user_email) VALUES(?, ?, ?, ?, ?, ?, ?)", (first_name,
+                                                                                                     last_name,
+                                                                                                     username,
+                                                                                                     password,
+                                                                                                     address,
+                                                                                                     phone_number,
+                                                                                                     user_email))
             conn.commit()
             response["message"] = "success"
             response["status_code"] = 201
         return response
 
-# inserting the products that are chosen
+# will insert products that will be sold
 
 
-@app.route('/products/', methods=["POST"])
+@app.route('/create-products/', methods=["POST"])
 @jwt_required()
 def create_products():
     response = {}
 
     if request.method == "POST":
-        title = request.form['title']
-        content = request.form['content']
-        date_created = datetime.datetime.now()
-
-        with sqlite3.connect('products.db') as conn:
+        product_name = request.form['product_name']
+        price = request.form['price']
+        description = request.form['description']
+        with sqlite3.connect('POS.db') as conn:
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO product("
-                           "title,"
-                           "content,"
-                           "date_created) VALUES(?, ?, ?)", (title, content, date_created))
+            cursor.execute("INSERT INTO products_table("
+                           "product_name,"
+                           "price,"
+                           "description) VALUES(?, ?, ?)", (product_name, price, description))
             conn.commit()
             response["status_code"] = 201
-            response['description'] = "Blog post added successfully"
+            response['description'] = "products created successfully"
         return response
 
+# Creating products
 
-@app.route('/get-products/', methods=["GET"])
-def get_products():
+
+@app.route('/products/')
+def display_products():
+    products = [{'id': 0, 'Product_name': 'Yocco speed point', 'Price': 300, 'Description': 'The best speed point'},
+                {'id': 1, 'Product_name': 'Yocco card machine', 'Price': 300, 'Description': 'Best card machine'},
+                {'id': 2, 'Product_name': 'samsung', 'Price': 300, 'Description': 'smartphone'}]
+    return jsonify(products)
+
+# getting the point of sales product from products
+
+
+@app.route('/get_Point_of_Sales/', methods=["GET"])
+def get_P_O_S():
     response = {}
-    with sqlite3.connect("products.db") as conn:
+    with sqlite3.connect("POS.db") as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM products ")
+        cursor.execute("SELECT * FROM products_table")
 
         posts = cursor.fetchall()
 
@@ -163,66 +190,69 @@ def get_products():
     response['data'] = posts
     return response
 
+# deleting of some products that will probably be out of stock or have gotten rotten
 
-@app.route("/delete-post/<int:post_id>")
+
+@app.route("/delete-products/<int:post_id>")
 @jwt_required()
-def delete_products(product_id):
+def delete_post(post_id):
     response = {}
-    with sqlite3.connect("products.db") as conn:
+    with sqlite3.connect("POS.db") as conn:
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM  WHERE id=" + str(product_id))
+        cursor.execute("DELETE FROM products_table WHERE id=" + str(post_id))
         conn.commit()
         response['status_code'] = 200
-        response['message'] = " product deleted successfully."
+        response['message'] = "products deleted successfully."
     return response
+
+# modifying some of the product prices and adding new products
 
 
 @app.route('/edit-post/<int:post_id>/', methods=["PUT"])
 @jwt_required()
-def update_post(product_id):
+def modify_post(post_id):
     response = {}
 
     if request.method == "PUT":
-        with sqlite3.connect('products.db') as conn:
+        with sqlite3.connect('POS.db') as conn:
             incoming_data = dict(request.json)
             put_data = {}
 
             if incoming_data.get("title") is not None:
                 put_data["title"] = incoming_data.get("title")
-                with sqlite3.connect('products.db') as conn:
+                with sqlite3.connect('POS.db') as conn:
                     cursor = conn.cursor()
-                    cursor.execute("UPDATE product SET title =? WHERE id=?", (put_data["title"], product_id))
+                    cursor.execute("UPDATE post SET title =? WHERE id=?", (put_data["title"], post_id))
                     conn.commit()
                     response['message'] = "Update was successfully"
                     response['status_code'] = 200
             if incoming_data.get("content") is not None:
                 put_data['content'] = incoming_data.get('content')
 
-                with sqlite3.connect('products.db') as conn:
+                with sqlite3.connect('POS.db') as conn:
                     cursor = conn.cursor()
-                    cursor.execute("UPDATE product SET content =? WHERE id=?", (put_data["content"], product_id))
+                    cursor.execute("UPDATE post SET content =? WHERE id=?", (put_data["content"], post_id))
                     conn.commit()
 
-                    response["content"] = "product updated successfully"
+                    response["content"] = "Content updated successfully"
                     response["status_code"] = 200
     return response
 
 
 @app.route('/get-post/<int:post_id>/', methods=["GET"])
-def get_post(product_id):
+def get_post(post_id):
     response = {}
 
-    with sqlite3.connect("products.db") as conn:
+    with sqlite3.connect("POS.db") as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM product WHERE id=" + str(product_id))
+        cursor.execute("SELECT * FROM post WHERE id=" + str(post_id))
 
         response["status_code"] = 200
-        response["description"] = "Products retrieved successfully"
+        response["description"] = "Point of Sale post retrieved successfully"
         response["data"] = cursor.fetchone()
 
     return jsonify(response)
 
 
-if __name__ == "__main__":
-    debug = True
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
